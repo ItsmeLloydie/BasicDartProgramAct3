a97bf411edd57a4a1de962c0fc0fd40874b6287e
ItsmeLloydie <168395482+ItsmeLloydie@users.noreply.github.com>
2025-08-08 05:05:51 +0000

    Dart simple calculator program

---
+++ Calculator.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  print("Simple Calculator");
+  
+  stdout.write("Enter first number: ");
+  double num1 = double.parse(stdin.readLineSync()!);
+  
+  stdout.write("Enter second number: ");
+  double num2 = double.parse(stdin.readLineSync()!);
+  
+  stdout.write("Enter operation (+, -, *, /): ");
+  String op = stdin.readLineSync()!;
+  
+  double result;
+  
+  if (op == '+') {
+    result = num1 + num2;
+  } else if (op == '-') {
+    result = num1 - num2;
+  } else if (op == '*') {
+    result = num1 * num2;
+  } else if (op == '/') {
+    if (num2 == 0) {
+      print("Cannot divide by zero.");
+      return;
+    }
+    result = num1 / num2;
+  } else {
+    print("Invalid operation.");
+    return;
+  }
+  
+  print("Result: $result");
+}
